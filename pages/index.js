869f62f443d35facc0cf10e97636b6a016977302
4eug <debraheug@gmail.com>
2022-10-01 13:42:18 +0000

    Main Index

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,15 @@
 import Hero from "../components/Hero";
+import HeroImage from "../components/HeroImage";
 import Meta from "../components/Meta";
 import Nav from "../components/Nav";
 
 export default function Home() {
   return (
-    <div className="bg-bg-color min-h-screen">
+    <section className="min-h-screen">
       <Meta title="Home" />
       <Nav />
       <Hero/>
-    </div>
+      <HeroImage/>
+    </section>
   );
 }
