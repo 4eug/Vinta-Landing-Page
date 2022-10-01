3bca63b049f9cba38f4ae7a3eed7b820401ef20f
4eug <debraheug@gmail.com>
2022-10-01 14:01:19 +0000

    Done

--- components/Meta.jsx
+++ components/Meta.jsx
@@ -1,19 +1,17 @@
 import Head from "next/head";
 
 export default function Meta({ title }) {
-  const siteTitle = "Vinta";
   const description = "Vintage Item Collections";
   const author = "Eugene";
-  const twitter = "@4eug";
-  const image =
-    "https://og-image.vercel.app/**Next.js**%20Starter%20%3Cbr%2F%3E%20%40exu3.png?theme=dark&md=1&fontSize=100px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fvercel-triangle-white.svg";
+  const twitter = "@4eug_";
+
   return (
     <Head>
       <meta charSet="utf-8" />
       <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width,initial-scale=1" />
       <title>
-        {title} | {siteTitle}
+        {title} 
       </title>
       <meta name="description" content={description} />
       <meta name="author" content={author} />
@@ -21,7 +19,6 @@ export default function Meta({ title }) {
       <meta property="og:type" content="website" />
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
-      <meta property="og:image" content={image} />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content={twitter} />
       <meta name="twitter:creator" content={twitter} />
